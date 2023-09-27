6b7b49ea4cbada09d7cf6a4c267b8e6c3126410d
srazap <shahidpeerzade4183@gmail.com>
2023-09-27 20:08:31 +0530

    added ftp changes

--- script.py
+++ script.py
@@ -1,1 +1,23 @@
-print('Hello Aayesha ;-)')
+import ftplib
+
+host = "66.220.9.50"
+username = "shahid22032000"
+password = "Shado7875"
+
+def read_config():
+    # read from config.json
+    # return config.json content in a dict
+    return ""
+
+def connect_ftp(host, username, password):
+    ftp = ftplib.FTP(host)
+    ftp.login(user=username, passwd=password)
+    return ftp
+
+def main():
+    ftp = connect_ftp(host, username, password)
+    dirList = ftp.dir()
+    print(dirList)
+
+if __name__ == "__main__":
+    main()
